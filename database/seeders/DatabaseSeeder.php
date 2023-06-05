a29029085e37b5ac7f11bb344808f42bcfafb7da
Deep Dev <snuppyy@gmail.com>
2023-06-05 17:31:25 +0500

    Install

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+   /**
+    * Seed the application's database.
+    */
+   public function run(): void
+   {
+      \App\Models\User::factory()->create([
+         'name' => 'Administrator',
+         'email' => 'admin@chimgan.uz',
+         'password' => Hash::make('395204055'),
+      ]);
+
+      \App\Models\User::factory(100)->create();
+   }
+}
